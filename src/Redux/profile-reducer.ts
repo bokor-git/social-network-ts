88f40a274b19b0340b8f4c351ad9449157010264
bokor-git <bogdankorolbk@gmail.com>
2020-05-15 18:33:41 +0300

    refactor thunk and action types in profile-reducer.ts

--- src/Redux/profile-reducer.ts
+++ src/Redux/profile-reducer.ts
@@ -1,13 +1,8 @@
 import {profileAPI} from "../api/api";
 import {stopSubmit} from "redux-form";
 import {ProfileDataType} from "../types/types"
-
-type ProfileActionsType = {
-    type: typeof SET_USER_PROFILE | typeof SET_STATUS_PROFILE | typeof SAVE_PHOTO_SUCCESS
-    profileData?: ProfileDataType
-    status?: string
-    photos?: string
-}
+import {ThunkAction} from "redux-thunk";
+import {AppStateType} from "./redux-store";
 
 
 const SET_USER_PROFILE = "SET_USER_PROFILE"
@@ -15,23 +10,36 @@ const SET_STATUS_PROFILE = "SET_STATUS_PROFILE"
 const SAVE_PHOTO_SUCCESS = "SAVE_PHOTO_SUCCESS"
 
 
-export const setProfileDate = (profileData: ProfileDataType): ProfileActionsType => ({
+type SetProfileDataActionType = {
+    type: typeof SET_USER_PROFILE
+    profileData: ProfileDataType
+}
+export const setProfileDate = (profileData: ProfileDataType): SetProfileDataActionType => ({
     type: SET_USER_PROFILE,
     profileData: profileData
 });
-export const setStatusProfile = (status: string): ProfileActionsType => ({type: SET_STATUS_PROFILE, status: status});
-export const savePhotoSuccess = (photos: string): ProfileActionsType => ({type: SAVE_PHOTO_SUCCESS, photos: photos});
-type InitialStateType = {
-    profileData: null | ProfileDataType
-    status: " " | string
-}
 
-let initialState:InitialStateType = {
+type SetStatusProfileActionType = {
+    type: typeof SET_STATUS_PROFILE
+    status:string
+}
+export const setStatusProfile = (status: string): SetStatusProfileActionType => ({type: SET_STATUS_PROFILE, status: status});
+
+type SavePhotoSuccessActionType = {
+    type: typeof SAVE_PHOTO_SUCCESS
+    photos: string
+}
+export const savePhotoSuccess = (photos: string): SavePhotoSuccessActionType => ({type: SAVE_PHOTO_SUCCESS, photos: photos});
+
+
+let initialState = {
     profileData: null as null | ProfileDataType,
     status: " " as " " | string
 };
 
+type InitialStateType = typeof initialState
 
+type ProfileActionsType = SetProfileDataActionType | SetStatusProfileActionType | SavePhotoSuccessActionType
 
 const myProfileReducer = (state = initialState, action: ProfileActionsType) => {
     switch (action.type) {
@@ -57,42 +65,49 @@ const myProfileReducer = (state = initialState, action: ProfileActionsType) => {
             return state
     }
 };
-export const getProfileInfo = (userId: number|null) => async (dispatch: any) => {
+
+type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ProfileActionsType>
+
+export const getProfileInfo = (userId: number|null):ThunkType => async (dispatch) => {
     const response = await profileAPI.getProfile(userId)
     dispatch(setProfileDate(response.data))
 
 };
 
-export const getProfileStatus = (userId: number) => async (dispatch: any) => {
+export const getProfileStatus = (userId: number):ThunkType => async (dispatch) => {
     const response = await profileAPI.getStatus(userId);
     dispatch(setStatusProfile(response.data))
 
 };
 
 
-export const updateProfileStatus = (status: string) => async (dispatch: any) => {
+export const updateProfileStatus = (status: string):ThunkType => async (dispatch) => {
     try {
         const response = await profileAPI.updateStatus(status);
         if (response.data.resultCode === 0)
             dispatch(setStatusProfile(status))
     } catch (error) {
-        debugger
         console.log(error.response.status);
     }
 };
-export const savePhoto = (file: File) => async (dispatch: any) => {
+export const savePhoto = (file: File):ThunkType => async (dispatch) => {
     const response = await profileAPI.savePhoto(file);
     if (response.data.resultCode === 0) {
         dispatch(savePhotoSuccess(response.data.data.photos))
     }
 };
 
-export const saveProfile = (profile: ProfileDataType) => async (dispatch: any, getState: any) => {
+type stopSubmitType = {
+    form:string
+    errors:any
+}
+export const saveProfile = (profile: ProfileDataType):ThunkType => async (dispatch, getState) => {
     let userId = getState().auth.userID;
     const response = await profileAPI.saveProfile(profile);
     if (response.data.resultCode === 0) {
         dispatch(getProfileInfo(userId))
     } else {
+        // @ts-ignore
         dispatch(stopSubmit("edit-profile", {_error: response.data.messages[0]}))
         return Promise.reject(response.data.messages[0])
     }
